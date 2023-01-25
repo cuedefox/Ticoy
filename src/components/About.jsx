510a3b9dd0201ec821ae18edf6dc7a311c486f60
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-01-25 20:25:02 -0300

    agrego fuentes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 
 const About = () => {
-    return <>
-    </>
+    return <div className="about">
+        <h2>Sobre Ticoy</h2>
+        <div className="about-item">
+            <h3>¿Quién esta detrás de la marca?</h3>
+            <p>Lorem ipsum dolor sit amet. Eos praesentium molestiae aut ipsa quae ex repellat omnis et dolorum rerum. Sed perspiciatis veritatis aut cumque neque eos assumenda repellat!</p>
+        </div>
+        <div className="about-item">
+            <h3>¿Qué significa Ticoy?</h3>
+            <p>Ticoy es un nombre y las personas llamadas asi se caracterizan por ser dinámicas, emprendedoras, comunicativas y salientes. Alegres, optimistas y portadoras de buen humor.</p>
+        </div>
+        <div className="about-item">
+            <h3>¿Cómo nació el emprendimiento?</h3>
+            <p>Lorem ipsum dolor sit amet. Eos praesentium molestiae aut ipsa quae ex repellat omnis et dolorum rerum. Sed perspiciatis veritatis aut cumque neque eos assumenda repellat!</p>
+        </div>
+    </div>
 }
 
 export default About;
