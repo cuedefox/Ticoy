77411cae7241cd2cc0443c91226058c05b16df27
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-01-24 21:00:15 -0300

    pruebo link whatsapp

--- src/components/WhatsappBut.jsx
+++ src/components/WhatsappBut.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import whatsapp from '../assets/img/whatsapp.svg';
 
 const WhatsappBut = () => {
+    const cargarImagen = require.context("../assets/imgitem", true);
     return <>
+        <a href=""><img src={cargarImagen(`./${whatsapp}`)} alt="" /></a>
     </>
 }
 
