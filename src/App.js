f6a3cad4fa9b582c3e13e3aa26be4bdafddcd55b
Rodrigo Vergara <rodrigovergara2001@gmail.com>
2023-01-26 14:53:07 -0300

    realizo navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import NavBar from './components/NavBar';
 import WhatsappBut from './components/WhatsappBut';
 import Main from './containers/Main';
 import './sass/import.scss';
 
 function App() {
   return <>
+    <NavBar />
     <Header />
     <Main />
     <Footer />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 const NavBar = () => {
-    return <>
-    </>
+    return <nav>
+        <ul>
+            <li><a href="#header">Inicio</a></li>
+            <li><a href="#products">Productos</a></li>
+            <li><a href="#faq">Preguntas frecuentes</a></li>
+            <li><a href="#about">Sobre ticoy</a></li>
+            <li><a href="#contact">Contacto</a></li>
+        </ul>
+    </nav>
 }
 
 export default NavBar;
