b02a7f8b2e5c68dcdd83574678fdcd953f61bb96
rosmar reyes <rosmarreyes8@gmail.com>
2023-01-29 12:32:39 -0300

    agregando seccion de productos

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,8 +1,41 @@
 import React from "react";
-
+import back from "../assets/img/backgrounds/productos.png";
+import anillos from "../assets/img/productos/anillos.jpeg"
+import pulseras from "../assets/img/productos/pulseras.jpeg"
+import collares from "../assets/img/productos/collares.jpeg"
+import aros from "../assets/img/productos/aros.jpeg"
 const Products = () => {
-    return <>
-    </>
+    const stylesBack = {
+        backgroundImage: `url(${back})`,
+    }
+    return (
+        <div className="productos" style={stylesBack} id="products">
+            <div className="tituloProductos">
+                <h2>Elegí tu Ticoy favorito</h2>
+                <p>Joyas de plata 925, acero quirúrgico plateado y dorado Desde Córdoba a todo el país.</p>
+            </div>
+
+            <div className="imagenesProductos">
+                <div className="aros">
+                    <img src={aros} alt="aros"/>
+                    <h3>AROS</h3>
+                </div>
+                <div className="collares">
+                    <img src={collares} alt="collares"/>
+                    <h3>COLLARES</h3>
+                </div>
+                <div className="anillos">
+                    <img src={anillos} alt="anillos"/>
+                    <h3>ANILLOS</h3>
+                </div>
+                <div className="pulseras">
+                    <img src={pulseras} alt="pulseras"/>
+                    <h3>PULSERAS</h3>
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
 export default Products;
