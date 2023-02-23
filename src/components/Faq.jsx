0a97abb99c97f499cd1f7e832ef367c101804bf2
rosmar reyes <rosmarreyes8@gmail.com>
2023-02-23 03:07:02 -0300

    optimizando modal

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -4,6 +4,10 @@ import Modal from "./ModalFaq";
 import icono from "../assets/img/iconFaq.png";
 import logo from "../assets/img/logoFaq.png";
 import backfaq from "../assets/img/backgrounds/faqs.svg"
+import logoWsp from "../assets/img/whatsapp.png"
+import mtdPago from "../assets/img/metodo-pago.png"
+import medidorAnillo from "../assets/img/medidor-anillos.png"
+
 const Faq = () => {
     const stylesBack = {
         backgroundImage: `url(${backfaq})`,
@@ -21,8 +25,11 @@ const Faq = () => {
     const buttonLeft = [
         //pregunta 1
         {
+            class: "whatsapp",
+            classDiv: "prg1",
             name: "¿Como realizo una compra?",
-            nameModal: "Las compras se realizan via Whatsapp",
+            nameModal: `Las compras se realizan via Whatsapp`,
+            img: logoWsp,
             abrirModal: openModal1,
             modalAbierto: isOpenModal1,
             cerrarModal: closeModal1,
@@ -31,13 +38,16 @@ const Faq = () => {
         //pregunta 2
         {
             name: "¿Cuáles son los métodos de pago?",
+            classDiv: "prg2",
             nameModal: "Efectivo, transferencia, débito y crédito. (Consultar promos vigentes)",
+            img: mtdPago,
             abrirModal: openModal2,
             modalAbierto: isOpenModal2,
             cerrarModal: closeModal2,
         },
         //pregunta 3
         {
+            classDiv: "imgNone",
             name: "¿Puedo retirar mi pedido personalmente?",
             nameModal: "Si! podes retirar tu pedido por la localidad de Salsipuedes, coordinando previamente día y horario por privado, al igual que la dirección.",
             abrirModal: openModal3,
@@ -46,6 +56,7 @@ const Faq = () => {
         },
         //pregunta 4
         {
+            classDiv: "imgNone",
             name: "¿Hacen envios?",
             nameModal: "Si!, si es por la zona donde resido.",
             abrirModal: openModal4,
@@ -57,8 +68,9 @@ const Faq = () => {
     const buttonRight = [
         //pregunta 5
         {
+            classDiv: "imgNone",
             name: "¿Las joyas tienen garantía? ",
-            nameModal: "Si, tienen garantía, pero solo las joyas provenientes de Córdoba Capital. ",
+            nameModal: `Si, tienen garantía, pero solo las joyas provenientes de Córdoba Capital.`,
             abrirModal: openModal5,
             modalAbierto: isOpenModal5,
             cerrarModal: closeModal5,
@@ -66,6 +78,7 @@ const Faq = () => {
 
         //pregunta 6
         {
+            classDiv: "imgNone",
             name: "¿Las joyas tienen cambio?",
             nameModal: "Depende cada caso particular. Por el tiempo que haya pasado desde la compra y el estado de la joya.",
             abrirModal: openModal6,
@@ -74,16 +87,19 @@ const Faq = () => {
         },
         //pregunta 7
         {
+            classDiv: "prg7",
             name: "¿Cómo calculo el talle de mi anillo?",
             nameModal: "Para saber tu talle, medí el diámetro interno de un anillo que ya tengas, para aproximarte al talle que buscás. Vas a medir la parte más ancha del anillo para asegurarte que pase por los nudillos sin problemas. Ahora resta comparar tu resultado con las medidas que te dejamos a continuación, recordá que no todos los dedos miden lo mismo. De igual forma ocurre al cambiar de mano.",
+            img: medidorAnillo,
             abrirModal: openModal7,
             modalAbierto: isOpenModal7,
             cerrarModal: closeModal7,
         },
         //pregunta 8
         {
+            classDiv: "imgNone",
             name: "¿Cómo cuido los productos?",
-            nameModal: "aksdjkdsjkds",
+            nameModal: `En nuestra seccion de "cuidado de tus joyas" conseguiras toda la data necesaria para mantener tus joyas como el primer dia`,
             abrirModal: openModal8,
             modalAbierto: isOpenModal8,
             cerrarModal: closeModal8,
@@ -103,7 +119,10 @@ const Faq = () => {
                                 </button>
 
                                 <Modal isOpen={button.modalAbierto} closeModal={button.cerrarModal}>
-                                    <h3>{button.nameModal}</h3>
+                                    <div className={button.classDiv}>
+                                    <p className="contenidoModal">{button.nameModal}</p>
+                                    <img src={button.img} alt="icono"className={button.class}/>
+                                    </div>
                                 </Modal>
                             </div>
                         ))
@@ -120,7 +139,10 @@ const Faq = () => {
                                 </button>
 
                                 <Modal isOpen={button.modalAbierto} closeModal={button.cerrarModal}>
-                                    <h3>{button.nameModal}</h3>
+                                    <div className={button.classDiv}>
+                                    <p className="contenidoModal">{button.nameModal}</p>
+                                    <img src={button.img} alt="icono" className={button.class}/>
+                                    </div>
                                 </Modal>
                             </div>
                         ))
